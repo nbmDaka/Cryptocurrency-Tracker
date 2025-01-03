8630085251fb7aa3fefd99f84b7dc8a43448e0dc
Nurdaulet <nurdaulet.beketov.2005@gmail.com>
2025-01-04 00:34:57 +0500

    Adding caching library alru cache

--- src/http_client.py
+++ src/http_client.py
@@ -1,4 +1,6 @@
 from aiohttp import ClientSession
+from async_lru import alru_cache
+
 
 class HTTPClient:
     def __init__(self, base_url: str, api_key: str):
@@ -12,11 +14,13 @@ class HTTPClient:
 # pro-api.coinmarketcap.com
 
 class CMCHTTPClient(HTTPClient):
+    @alru_cache
     async def get_listings(self):
         async with self._session.get("/v1/cryptocurrency/listings/latest") as resp:
             result =  await resp.json()
             return result["data"]
 
+    @alru_cache
     async def get_currency(self, currency_id: int):
         async with self._session.get(
                 "/v2/cryptocurrency/quotes/latest",
